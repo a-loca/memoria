3dad787e999ec2705620e0d7ac7b6e7970c85de4
Alessandro Locatelli <alessandro.locatelli02@gmail.com>
2026-01-20 23:54:59 +0100

    style: improve animation for gallery list of pics

--- src/components/ListLayout/ListLayout.jsx
+++ src/components/ListLayout/ListLayout.jsx
@@ -1,42 +1,74 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import styles from "./ListLayout.module.css";
 import { getFormattedDate } from "../../utils/utils";
 import gsap from "gsap";
 
 function ListLayout({ pictures }) {
-  const [hovered, setHovered] = useState(0);
-  const highlight = useRef(null);
+  const animateHighlightIn = (e) => {
+    const div = e.target;
+    const rect = div.getBoundingClientRect();
 
-  const handleHover = (e, i) => {
-    setHovered(i);
-    const rect = e.target.getBoundingClientRect();
-    const padding = 24;
-    console.log(rect);
-    gsap.to(highlight.current, { height: rect.height + padding, top: rect.top - padding / 2 });
+    // -1 if mouse entering from the top, 1 if from the bottom
+    const direction = e.clientY < rect.top + rect.height / 2 ? -1 : 1;
+
+    const highlight = div.querySelector(`.${styles.highlight}`);
+    const info = [div.querySelector(`.${styles.date}`), div.querySelector(`.${styles.user}`)];
+
+    gsap.fromTo(highlight, { top: 100 * direction + "%" }, { top: 0, duration: 0.5 });
+    gsap.fromTo(
+      info,
+      { opacity: 0, filter: "blur(0.25rem)" },
+      { opacity: 1, filter: "blur(0rem)", duration: 0.3 }
+    );
+  };
+
+  const animateHighlightOut = (e) => {
+    const div = e.target;
+    const rect = div.getBoundingClientRect();
+
+    // 1 if exiting from the bottom, -1 if from the top
+    const direction = e.clientY < rect.top + rect.height / 2 ? -1 : 1;
+
+    const highlight = div.querySelector(`.${styles.highlight}`);
+    const info = [div.querySelector(`.${styles.date}`), div.querySelector(`.${styles.user}`)];
+
+    gsap.to(highlight, {
+      top: 100 * direction + "%",
+      duration: 0.5,
+    });
+    gsap.fromTo(
+      info,
+      { opacity: 1, filter: "blur(0rem)" },
+      { opacity: 0, filter: "blur(0.25rem)", duration: 0.3 }
+    );
   };
 
   return (
     <div className={styles.list}>
-      <div ref={highlight} className={styles.highlight} />
-      {pictures.map((pic, i) => {
+      {pictures.map((pic) => {
         return (
           pic.description && (
-            <div key={pic.id} className={styles.row} onMouseEnter={(e) => handleHover(e, i)}>
-              {hovered === i && (
-                <div className={styles.date}>
-                  <span>{getFormattedDate(pic.created_at)}</span>
-                </div>
-              )}
+            <div
+              key={pic.id}
+              className={styles.row}
+              onMouseEnter={(e) => animateHighlightIn(e)}
+              onMouseLeave={(e) => animateHighlightOut(e)}
+            >
+              <div className={styles.highlight} />
+
+              <div className={styles.date}>
+                <span>{getFormattedDate(pic.created_at)}</span>
+              </div>
+
               <div className={styles.description}>
                 <a>
                   <span className={styles.description}>{pic.description}</span>
                 </a>
               </div>
-              {hovered === i && (
-                <div className={styles.user}>
-                  <span>by {pic.user.name}</span>
-                </div>
-              )}
+
+              <div className={styles.user}>
+                <span>by {pic.user.name}</span>
+              </div>
             </div>
           )
         );
--- src/components/ScrollToTopButton/ScrollToTopButton.jsx
+++ src/components/ScrollToTopButton/ScrollToTopButton.jsx
@@ -4,7 +4,7 @@ import styles from "./ScrollToTopButton.module.css";
 function ScrollToTopButton() {
   return (
     <button onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}>
-      <span>Back to top</span>
+      <span>↑ Back to top</span>
     </button>
   );
 }
