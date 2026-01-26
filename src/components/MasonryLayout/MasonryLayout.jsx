6c14fbf8b13fff278bc33d5b26508deb2388e691
Alessandro Locatelli <alessandro.locatelli02@gmail.com>
2026-01-26 12:07:09 +0100

    fix: trying out masonry layout using react-responsive-masonry instead of css columns

--- src/components/MasonryLayout/MasonryLayout.jsx
+++ src/components/MasonryLayout/MasonryLayout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./MasonryLayout.module.css";
 import Picture from "../Picture/Picture";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
 function MasonryLayout({ pictures }) {
   return (
     <div className={styles.container}>
-      <div className={styles.masonry}>
+      {/* 
+       <div className={styles.masonry}>
         {pictures.map((pic) => {
 
           return (
@@ -15,6 +17,22 @@ function MasonryLayout({ pictures }) {
           );
         })}
       </div>
+       */}
+      <ResponsiveMasonry
+        className={styles.masonry}
+        columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3, 1200: 4 }}
+        // gutterBreakPoints={{ 350: "12px", 750: "16px", 900: "2em" }}
+      >
+        <Masonry sequential={true}>
+          {pictures.map((pic) => {
+            return (
+              <div key={pic.id} className={styles.imgWrapper}>
+                <Picture src={pic.urls.small} blurhash={pic.blurhash} alt={pic.description} />
+              </div>
+            );
+          })}
+        </Masonry>
+      </ResponsiveMasonry>
     </div>
   );
 }
